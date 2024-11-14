31b67fc54ce89366dac8dabf59baa8f35ac6deed
Celina MB <103222853+celina-mb@users.noreply.github.com>
2024-11-13 18:06:03 -0600

    i will keep on editing this

--- Course.cpp
+++ Course.cpp
@@ -1,5 +1,32 @@
 #include "Course.h"
 
+    int getCourseKey();
+    int getCourseDuration();
+    std::string getCourseName();
+    Lab getCourseLab();
+    vector<Student> getCourseStudents()
+
+
+int Course::getCourseKey()
+{
+    return courseKey;
+}
+
+void Course::setTeacher(Teacher t1)
+{
+    courseTeacher=t1;
+}
+void Course::addStudent(Student s1)
+{
+    courseStudents.push_back(s1);
+}
+
+void Course::removeStudent(string id)
+{
+    for(int i=0;)
+        //investigar como se hace esto
+}
+
 std::string Course::courseDetails()
 {
     return std::string();
