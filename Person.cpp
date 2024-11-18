43cde36ba2f41717b5b93d6d92a6e3f587e95516
Celina MB <103222853+celina-mb@users.noreply.github.com>
2024-11-17 22:25:58 -0600

    updated person.cpp with all methods

--- Person.cpp
+++ Person.cpp
@@ -1,5 +1,37 @@
 #include "Person.h"
+#include <string>
+using namespace std;
 
-Person::Person(std::string name, std::string lastName, std::string address, int age)
-{
+Person::Person(string _name, int _age, string _address){
+  name=_name;
+  age=_age;
+  address=_address;
+}
+
+string Person::getName(){
+  return name;
+}
+
+int Person::getAge(){
+  return age;
+}
+
+string Person::getAddress(){
+  return address;
+}
+
+void Person::setName(string _name) {
+    name=_name;
+}
+
+void Person::setAge(int _age) {
+    age=_age;
+}
+
+void Person::setAddress(string _address) {
+    address=_address;
+}
+
+string Person::getPersonDetails() {
+    return "Name: " + name + ", Age: " + to_string(age) + ", Address: " + address;
 }
