2a18f120d0844c1acfd7481dd0dfcc6b7a65150d
Celina MB <103222853+celina-mb@users.noreply.github.com>
2024-11-17 23:25:32 -0600

    updated Teacher.cpp file with methods, constructor

--- Teacher.cpp
+++ Teacher.cpp
@@ -2,9 +2,24 @@
 #include <string>
 using namespace std;
 
-Teacher::Teacher(string _name, int _age, string _address, string _teacherId, string _teachingHours){
-  :Person(_name, _age, _address)
+Teacher::Teacher(string _name, int _age, string _address, string _teacherId, string _teachingHours)
+  :Person(_name, _age, _address){
   teacherID = _teacherID;
   teachingHours = _teachingHours;
 }
 
+string Teacher::getTeacherID(){
+    return teacherID;
+}
+
+int Teacher::getTeachingHours(){
+    return teachingHours;
+}
+
+void Teacher::setTeachingHours(int _teachingHours) {
+    teachingHours = _teachingHours;
+}
+
+string Teacher::getTeacherDetails(){
+    return getPersonDetails() + ", TeacherID: " + teacherID + ", Teaching Hours: " + to_string(teachingHours);
+}
