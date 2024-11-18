7e22545335aa2ff0abc40bfb1415b329fa35f227
Celina MB <103222853+celina-mb@users.noreply.github.com>
2024-11-17 22:38:35 -0600

    updated Student.h with all the methods and constructor

--- Student.h
+++ Student.h
@@ -1,16 +1,26 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-
 using namespace std;
 #include <string>
 
-class Student{
+#include "Person.h" //because of inheritance 
+class Student:public Person
+{
     private:
-    string studentId;
+    string studentID;
     int semester;
+    double avrg;
 
     public:
-    Student(string studentId, int semester)
+    Student(string name, int age, string address, string studentID, int semester, double avrg)
+    
+    string getStudentID();
+    int getSemester();
+    int getAvrg();
+
+    void setSemester(int _semester);
+    void setAvrg(double _avrg)
+
     string getStudentDetails();
 };
 
