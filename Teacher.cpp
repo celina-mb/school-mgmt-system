db17b62d1fbcbe0984a9e5d6b3f2cd26903d212c
Celina MB <103222853+celina-mb@users.noreply.github.com>
2024-11-17 23:21:18 -0600

    Update Teacher.cpp

--- Teacher.cpp
+++ Teacher.cpp
@@ -1,6 +1,10 @@
 #include "Teacher.h"
+#include <string>
+using namespace std;
 
-Teacher::Teacher(std::string teacherId, std::string courseKey)
-{
+Teacher::Teacher(string _name, int _age, string _address, string _teacherId, string _teachingHours){
+  :Person(_name, _age, _address)
+  teacherID = _teacherID;
+  teachingHours = _teachingHours;
 }
 
