3f4f8b07e325ac5e1a5eb447d350224fcdf199c8
Celina MB <103222853+celina-mb@users.noreply.github.com>
2024-11-17 23:03:50 -0600

    Student.cpp updated for constructor & methods

--- Student.cpp
+++ Student.cpp
@@ -1,6 +1,37 @@
 #include "Student.h"
+#include <string>
+using namespace std;
 
-string Student::getStudentDetails()
+Student::Student(string _name, int _age, string _address, string _studentID, int _semester, double _avrg)
 {
-    return string();
+    name=_name;
+    age=_age;
+    address=_address;
+    studentID=_studentID;
+    semester=_semester;
+    avrg=_avrg;
+}
+
+string Student::getStudentID(){
+    return studentID;
+}
+
+int Student::getSemester(){
+    return semester;
+}
+
+int Student::getAvrg(){
+    return avrg;
+
+void Student::setSemester(int _semester){
+    semester=_semester;
+}
+
+void Student::setAvg(int _avrg){
+avg=_avg;
+}
+
+string Student::getStudentDetails(){
+    return getPersonDetails() + ", StudentID: " + studentID + ", Semester: " + 
+           to_string(semester) + ", Average: " + to_string(avrg);
 }
