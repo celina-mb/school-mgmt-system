8772429d988059b373c29dd21d0cf0ce9760003a
Celina MB <103222853+celina-mb@users.noreply.github.com>
2024-11-22 11:54:44 -0600

    commented code

--- Student.h
+++ Student.h
@@ -4,14 +4,14 @@ using namespace std;
 #include <string>
 
 #include "Person.h" //because of inheritance 
-class Student:public Person
+class Student:public Person //it takes the protected attributes from the Person superclass
 {
     private:
     string studentID;
     int semester;
     double avrg;
 
-    public:
+    public: //constructor, getters and setters
     Student(string name, int age, string address, string studentID, int semester, double avrg)
     
     string getStudentID();
