43f13c5d6250614d488fd9ce7731b7e7ea9b12be
Celina MB <103222853+celina-mb@users.noreply.github.com>
2024-11-23 08:14:39 -0600

    Update Student.h

--- Student.h
+++ Student.h
@@ -4,24 +4,26 @@ using namespace std;
 #include <string>
 
 #include "Person.h" //because of inheritance 
-class Student:public Person //it takes the protected attributes from the Person superclass
-{
+class Student:public Person {
     private:
-    string studentID;
-    int semester;
-    double avrg;
+        string studentID;
+        int semester;
+        double avrg;
 
-    public: //constructor, getters and setters
-    Student(string name, int age, string address, string studentID, int semester, double avrg)
-    
-    string getStudentID();
-    int getSemester();
-    int getAvrg();
+    public: //constructor
+        Student(string name, int age, string address, string studentID, int semester, double avrg)
 
-    void setSemester(int _semester);
-    void setAvrg(double _avrg)
+        //getters
+        string getStudentID();
+        int getSemester();
+        int getAvrg();
 
-    string getStudentDetails();
+        //setters
+        void setSemester(int _semester);
+        void setAvrg(double _avrg)
+
+        //special method to retrieve all the student data as a string
+        string getStudentDetails();
 };
 
 #endif
