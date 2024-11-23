65c03340bed65f3fd2048ab68108c60682d92bc2
Celina MB <103222853+celina-mb@users.noreply.github.com>
2024-11-23 07:43:53 -0600

    Update Lab.cpp

--- Lab.cpp
+++ Lab.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 #include <string>
 
-Lab::Lab(int _labCode, string _labType, int _labSize){
-  labCode=_labCode;
-  labType=_labType;
-  labSize=_labSize;
-}
+//constructor
+Lab::Lab(int _labCode, string _labType, int _labSize)
+  : labCode(_labCode), labType(_labType), labSize(_labSize) {}
 
+//getters
 int Lab::getLabCode(){
     return labCode;
 }
@@ -20,10 +19,12 @@ int Lab::getLabSize(){
     return labSize;
 }
 
+//setter
 void Lab::setLabSize(int _labSize) {
     labSize=_labSize;
 }
 
+// returns all lab data as a string
 string Lab::getLabDetails(){
     return "Lab Code: " + to_string(labCode) + ", Lab Type: " + labType + ", Lab Size: " + to_string(labSize);
 }
